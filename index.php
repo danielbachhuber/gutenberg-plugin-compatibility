b5acaffc82bad11531c095c0c44878c5fed1456a
Daniel Bachhuber <daniel@handbuilt.co>
2018-02-06 15:57:14 -0800

    Add GA tag

--- index.php
+++ index.php
@@ -29,6 +29,16 @@
 			padding-right: 0;
 		}
 	</style>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-49623239-2"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-49623239-2');
+	</script>
+
 </head>
 <body>
 
